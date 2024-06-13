22f2c84764e5e6ed3ccbd3edc5f6c38a5d2dfa46
kalosius <kalosius43@gmail.com>
2024-06-13 13:41:01 +0300

    initial commit

--- ecom/settings.py
+++ ecom/settings.py
@@ -2,10 +2,11 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Load our environmental variables
+# Load environmental variables
 load_dotenv()
 
 
