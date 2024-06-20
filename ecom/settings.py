885db11e8498d13edf9a7ee7164c38c2f760d5ad
kalosius <kalosius43@gmail.com>
2024-06-20 10:56:54 +0300

    debug

--- ecom/settings.py
+++ ecom/settings.py
@@ -17,7 +17,7 @@ load_dotenv()
 SECRET_KEY = 'django-insecure-x4m$gfeda-r+)u05g*bzm%8#_vz&8-wl^3epo45gqi#_eqwvtq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-ecom-production-7df9.up.railway.app', 'https://django-ecom-production-7df9.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://django-ecom-production-7df9.up.railway.app']
