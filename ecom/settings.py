c555bf1c3d61deaf9de56cdfd5774cb2647d54d2
kalosius <kalosius43@gmail.com>
2024-07-02 21:33:43 +0300

    dependency updated

--- ecom/settings.py
+++ ecom/settings.py
@@ -124,16 +124,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 # White noise static stuff
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+
 
 
 # Default primary key field type
--- ecom/urls.py
+++ ecom/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import settings
+from django.conf import settings
 from django.conf.urls.static import static
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
